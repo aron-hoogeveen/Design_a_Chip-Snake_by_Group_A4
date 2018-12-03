33fcbc3fdf1f3053674af1764d5d367930488d6f
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2018-12-03 12:30:28 +0100

    Update something

--- ItemGenerator/VHDL/itemGenerator.vhd
+++ ItemGenerator/VHDL/itemGenerator.vhd
@@ -8,6 +8,7 @@ entity itemGenerator is
         item_ok       : in  std_logic;
         rng_in        : in  std_logic;
         count_done    : in  std_logic;
+        new_head_loc  : in  std_logic_vector(9 downto 0);   -- These 10 bits are the first 10 bits from the 12 bits of new_head output from the snake
         count_start   : out std_logic;
         storage_enable: out std_logic;
         ack_req       : out std_logic;
