8330c874b4545e2faa6bc8825be971d380163343
Pelle Wiersma <pelle.wiersma@gmail.com>
2019-01-09 21:11:19 +0100

    Added new RNG (yet again)
    This RNG can be synthesised and routed but has less possible coordinates to generate. A bit (e.g. part of the head coordinate) can be connected to increase the amount of possible coordinates considerable but for now and testing this should suffice.

--- ItemGenerator/VHDL/rng-behaviour.vhd
+++ ItemGenerator/VHDL/rng-behaviour.vhd
@@ -2,42 +2,30 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of rng is
-	component xor2 is
-		port(a, b: in std_logic; z: out std_logic);
-	end component;
-	component inverter is
-		port(a: in std_logic; z: out std_logic);
-	end component;
-	component mux is
-		port(a, b, x: in std_logic; z: out std_logic);
-	end component;
-signal s1 : std_logic := '1';
-signal s2 : std_logic := '1';
-signal s3, s4, i1, i2, i3, i4, i5, i6, i7, i8, ring_out, init: std_logic;
+component xor2
+is
+    port(a, b : in std_logic; z : out std_logic);
+  end component;
+component ff
+is
+    port(d, clk, reset : in std_logic; q : out std_logic);
+  end component;
+component or2 is
+port(a, b : in std_logic; z : out std_logic);
+end component;
+  signal s0, s1, s2, s3, s4, s5: std_logic;
 begin
-	inv9: inverter port map (reset, init);
-
-	inv1: inverter port map (s3, i1);
-	inv2: inverter port map (i1, i2);
-	inv3: inverter port map (i2, s1);
-
-	inv4: inverter port map (s4, i4);
-	inv5: inverter port map (i4, i5);
-	inv6: inverter port map (i5, i6);
-	inv7: inverter port map (i6, i7);
-	inv8: inverter port map (i7, s2);
-
-	mux1: mux port map (init, s1, init, s3);
-
-	mux2: mux port map (init, s2, init, s4);
-
-	lblxor: xor2 port map (s3, s4, ring_out);
-
-	process(clk)
-	begin
-		if(clk = '1') then
-			z <= ring_out;
-		end if;
-	end process;
+  --i1 : ff port map(d => s0, clk => clk, reset => reset, q => s1);
+  process (clk) 
+begin
+	if(rising_edge(clk)) then
+		s1 <= s0;
+	end if;
+end process;
+  sigxor2 : xor2 port map(a => s1, b => z, z => s2);
+  i2 : ff port map(d => s2, clk => clk, reset => reset, q => s3);
+  i3 : ff port map(d => s3, clk => clk, reset => reset, q => z);
+  i6 : or2 port map(a => reset, b => z, z => s0);
 end behaviour;
+
 
