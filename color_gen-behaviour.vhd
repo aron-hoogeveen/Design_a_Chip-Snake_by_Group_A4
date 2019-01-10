9d502fed77010ab8bcfe8cff4732b601719b2862
ossamatudelft <44971466+ossamatudelft@users.noreply.github.com>
2019-01-10 14:37:30 +0100

    Updated unfinished color_gen.vhd
    This is an updated version of color_gen.vhd including the changes regarding the power-ups, it is unfinished though!

--- color_gen-behaviour.vhd
+++ color_gen-behaviour.vhd
@@ -2,11 +2,11 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of color_gen is
-type color_state is (field_state, snake_state, item_state);
+type color_state is (field_state, snake_state, food_state, speed_state, invert_state, flicker_state);
 
 signal state, next_state : color_state;
 begin
-	process(state, video_on, snake, item)
+	process(state, video_on, snake, item, item_type)
 	begin
 		case state is
 
@@ -15,9 +15,18 @@ begin
 				green	<= '0';
 				blue <= '0';
 				if (snake = '1') then next_state <= snake_state;
-					elsif(item = '1' and snake = '0') then next_state <= item_state;
-						else next_state <= state;
+					elsif(item = '1' and snake = '0') 
+						if(item_type = "00") then next_state <= food_state;
+						elsif(item_type = "01") then next_state <= speed_state;
+						elsif(item_type = "10") then next_state <= invert_state;
+						else next_state <= flicker_state;
+						end if;
+				else next_state <= state;
 				end if;
+
+
+
+
 			
 			when snake_state => 
 				red	<= '0';
@@ -28,7 +37,7 @@ begin
 						else next_state <= state;
 				end if;
 
-			when item_state => 
+			when food_state => 
 				red	<= '1';
 				green	<= '0';
 				blue <= '0';
@@ -37,8 +46,35 @@ begin
 						else next_state <= state;
 				end if;
 
-			when others =>
+			when speed_state => 
+				red	<= '0';
+				green	<= '1';
+				blue <= '0';
+				if (snake = '1') then next_state <= snake_state;
+					elsif(item = '0' and snake = '0') then next_state <= field_state;
+						else next_state <= state;
+				end if;
+
+			when invert_state => 
+				red	<= '0';
+				green	<= '1';
+				blue <= '1';
+				if (snake = '1') then next_state <= snake_state;
+					elsif(item = '0' and snake = '0') then next_state <= field_state;
+						else next_state <= state;
+				end if;
+
+			when flicker_state => 
 				red	<= '1';
+				green	<= '0';
+				blue <= '1';
+				if (snake = '1') then next_state <= snake_state and flicker <= '1';
+					elsif(item = '0' and snake = '0') then next_state <= field_state;
+						else next_state <= state;
+				end if;
+
+			when others =>
+				red	<= '0';
 				green	<= '0';
 				blue <= '0';
 				next_state <= field_state;
