5c9a044aff668f317773685054d314c5fc69f5c9
ossamatudelft <44971466+ossamatudelft@users.noreply.github.com>
2019-01-10 14:32:34 +0100

    updated color_gen.vhd including power-ups
    This is an updated version of color_gen.vhd including the changes regarding the power ups

--- color_gen.vhd
+++ color_gen.vhd
@@ -2,12 +2,13 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity color_gen is
-   port(clk      : in  std_logic;
-        reset    : in  std_logic;
-        snake    : in  std_logic;
-        item     : in  std_logic;
-        video_on : in  std_logic;
-        red      : out std_logic;
-        green    : out std_logic;
-        blue     : out std_logic);
+   port(clk       : in  std_logic;
+        reset     : in  std_logic;
+        snake     : in  std_logic;
+        item      : in  std_logic;
+		  item_type : in std_logic_vector(1 downto 0);
+        video_on  : in  std_logic;
+        red       : out std_logic;
+        green     : out std_logic;
+        blue      : out std_logic);
 end color_gen;
