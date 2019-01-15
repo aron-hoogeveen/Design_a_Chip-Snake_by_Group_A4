f2bd11282197e7f653d3da4c16c1fdeba1163ba4
Pelle Wiersma <pelle.wiersma@gmail.com>
2019-01-15 14:48:06 +0100

    Added most recent version of Snake_struct

--- Snake/VHDL/snake_struct.vhd
+++ Snake/VHDL/snake_struct.vhd
@@ -10,10 +10,8 @@ entity snake_struct is
         crn_clr_flag   : in  std_logic;
         ig_item_loc    : in  std_logic_vector(9 downto 0);
         ig_loc_set     : in  std_logic;
-        st_item_loc    : in  std_logic_vector(9 downto 0);
-        st_item_clear  : in  std_logic;
-        st_item_exists : in  std_logic;
-        st_item_type   : in  std_logic_vector(1 downto 0);
+        st_item_one    : in  std_logic_vector(11 downto 0);
+        st_item_two    : in  std_logic_vector(11 downto 0);
         x_range        : in  std_logic_vector(9 downto 0);
         y_range        : in  std_logic_vector(9 downto 0);
         tail           : in  std_logic;
@@ -23,13 +21,13 @@ entity snake_struct is
         new_head_flag  : out std_logic;
         length         : out std_logic_vector(5 downto 0);
         ig_loc_clear   : out std_logic;
+        ig_item_clear   : in std_logic;
         ig_item_ok     : out std_logic;
-        st_item_set    : out std_logic;
-        st_item_no     : out std_logic;
+        ig_item_set     : out std_logic;
+        ig_item_type   : out std_logic;
         so_range_clear : out std_logic;
-        so_reset       : out std_logic;
         new_head       : out std_logic_vector(11 downto 0);
-	gr_flickering_clear	: in std_logic;
-	gr_flickering_set		 : out std_logic);
+	    gr_flickering_set	: out std_logic;
+        move_out            : out std_logic);
 end snake_struct;
 
