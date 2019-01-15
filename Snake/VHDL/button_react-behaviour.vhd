cc5009902e20409b3c036d4d347cc90841560da8
Pelle Wiersma <pelle.wiersma@gmail.com>
2019-01-15 10:11:04 +0100

    Converted b_r to Moore machine

--- Snake/VHDL/button_react-behaviour.vhd
+++ Snake/VHDL/button_react-behaviour.vhd
@@ -4,9 +4,10 @@ use IEEE.numeric_std.all;
 
 architecture behaviour of button_react is
 
-type button_react_state is (idle, calc_new_head, set_flags, set_corner_flags, clr_flags, goLeft, goRight, goUp, goDown, waitHead, waitCor, sendChc, sendChcCorner);
+type button_react_state is (idle, set_flags, waitHead, waitCor, sendChc, waitMove);
 signal state, next_state: button_react_state;
-signal ytoInt, xtoInt: signed(4 downto 0);
+signal ytoInt, xtoInt: unsigned(4 downto 0);
+signal corner_exists : std_logic;
 
 begin
 
@@ -32,6 +33,62 @@ end process;
 --	end if;
 --end process;
 
+
+process (buttons, head)
+
+begin
+
+	xtoInt <= unsigned(head(11 downto 7));
+	ytoInt <= unsigned(head(6 downto 2) );
+		
+	if buttons = head(1 downto 0) or (buttons = "01" and head(1 downto 0) = "10") or (buttons = "10" and head(1 downto 0) = "01") or (buttons = "11" and head(1 downto 0) = "00") or (buttons = "00" and head(1 downto 0) = "11")then
+		corner_exists <= '0';
+		corner <= "000000";
+		if buttons = "00" then
+			new_head <= std_logic_vector(xtoInt + 1) & head(6 downto 2) & buttons;
+		elsif buttons = "01" then
+			new_head <= head(11 downto 7) & std_logic_vector(ytoInt + 1) & buttons;
+		elsif buttons = "11" then
+			new_head <= std_logic_vector(xtoInt - 1) & head(6 downto 2) & buttons;
+		elsif buttons = "10" then
+			new_head <= head(11 downto 7) & std_logic_vector(ytoInt - 1) & buttons;
+		else
+			new_head <= head(11 downto 0);
+		end if;
+	else
+		corner_exists <= '1';
+		if(inversion = '0') then
+			if buttons = "00" then
+				new_head <= std_logic_vector(xtoInt + 1) & head(6 downto 2) & "00";
+				corner <= head(11 downto 7) & '0';
+			elsif buttons = "01" then
+				new_head <= head(11 downto 7) & std_logic_vector(ytoInt + 1) & "01";
+				corner <= head(6 downto 2) & '1';
+			elsif buttons = "11" then
+				new_head <= std_logic_vector(xtoInt - 1) & head(6 downto 2) & "11";
+				corner <= head(11 downto 7) & '0';
+			else
+				new_head <= head(11 downto 7) & std_logic_vector(ytoInt - 1) & "10";
+				corner <= head(6 downto 2) & '1';
+			end if;
+		else
+			if buttons = "00" then
+				new_head <= std_logic_vector(xtoInt - 1) & head(6 downto 2) & "11";
+				corner <= head(11 downto 7) & '0';
+			elsif buttons = "01" then
+				new_head <= head(11 downto 7) & std_logic_vector(ytoInt - 1) & "10";
+				corner <= head(6 downto 2) & '1';
+			elsif buttons = "11" then
+				new_head <= std_logic_vector(xtoInt + 1) & head(6 downto 2) & "00";
+				corner <= head(11 downto 7) & '0';
+			else
+				new_head <= head(11 downto 7) & std_logic_vector(ytoInt + 1) & "01";
+				corner <= head(6 downto 2) & '1';
+			end if;
+		end if;			
+	end if;
+end process;
+
 process (state, move, buttons, head, new_head_clr_flag, corner_clr_flag, chc_clr_flag, ytoInt, xtoInt, inversion, head_ok)
 
 begin
@@ -40,169 +97,42 @@ case state is
 
 	when idle =>
 	
-		new_head <= "000000000000";
-		corner <= "000000";
 		corner_flag <= '0';
 		new_head_flag <= '0';
 		chc_flag <= '0';
 
 		if move = '1' then
-			next_state <= calc_new_head;
+			next_state <= sendChc;
 		else
 			next_state <= idle;
-		end if;
-
-	when calc_new_head =>
-	
-		corner <= "000000";
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '0';
-		xtoInt <= signed(head(11 downto 7) );
-		ytoInt <= signed(head(6 downto 2) );
-		
-		if buttons = head(1 downto 0) or (not head(1 downto 0)) = buttons then
-			if buttons = "00" then
-				new_head <= std_logic_vector(xtoInt + 1) & head(6 downto 2) & buttons;
-			elsif buttons = "01" then
-				new_head <= head(11 downto 7) & std_logic_vector(ytoInt + 1) & buttons;
-			elsif buttons = "11" then
-				new_head <= std_logic_vector(xtoInt - 1) & head(6 downto 2) & buttons;
-			elsif buttons = "10" then
-				new_head <= head(11 downto 7) & std_logic_vector(ytoInt - 1) & buttons;
-			else
-				new_head <= head(11 downto 0);
-			end if;
-			next_state <= sendChc;
-		else 
-			new_head <= "000000000000";
-			if(inversion = '0') then
-				if buttons = "00" then
-					next_state <= goRight;
-				elsif buttons = "01" then
-					next_state <= goUp;
-				elsif buttons = "11" then
-					next_state <= goLeft;
-				else
-					next_state <= goDown;
-				end if;
-			else
-				if buttons = "00" then
-					next_state <= goLeft;
-				elsif buttons = "01" then
-					next_state <= goDown;
-				elsif buttons = "11" then
-					next_state <= goRight;
-				else
-					next_state <= goUp;
-				end if;
-			end if;			
-		end if;
+		end if;		
 
 	when sendChc =>
 
 		corner_flag <= '0';
 		new_head_flag <= '0';
 		chc_flag <= '1';
-		new_head <= new_head;
-		corner <= corner;
 
 		if(chc_clr_flag = '1') then
 			if(head_ok = '1') then
 				next_state <= set_flags;
 			else
-				next_state <= idle;
+				next_state <= waitMove;
 			end if;
 		else
 			next_state <= sendChc;
 		end if;
-
-	when sendChcCorner =>
-
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '1';
-		new_head <= new_head;
-		corner <= corner;
-
-		if(chc_clr_flag = '1') then
-			if(head_ok = '1') then
-				next_state <= set_corner_flags;
-			else
-				next_state <= idle;
-			end if;
-		else
-			next_state <= sendChcCorner;
-		end if;
-
-	when goRight =>
-	
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '0';
-		new_head <= std_logic_vector(xtoInt + 1) & head(6 downto 2) & "00";
-		corner <= head(11 downto 7) & '0';
-
-		next_state <= sendChcCorner;
-		
-	when goUp =>
-	
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '0';
-		new_head <= head(11 downto 7) & std_logic_vector(ytoInt + 1) & "01";
-		corner <= head(6 downto 2) & '1';
-
-		next_state <= sendChcCorner;
-	
-	when goLeft =>
-	
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '0';
-		new_head <= std_logic_vector(xtoInt - 1) & head(6 downto 2) & "11";
-		corner <= head(11 downto 7) & '0';
-
-		next_state <= sendChcCorner;
-	
-	when goDown =>
-	
-		corner_flag <= '0';
-		new_head_flag <= '0';
-		chc_flag <= '0';
-		new_head <= head(11 downto 7) & std_logic_vector(ytoInt - 1) & "10";
-		corner <= head(6 downto 2) & '1';
-
-		next_state <= sendChcCorner;
 		
 	when set_flags =>
 		
-		corner_flag <= '0';
+		corner_flag <= corner_exists;
 		new_head_flag <= '1';
 		chc_flag <= '0';
-		new_head <= new_head;
-		corner <= corner;
-		
-		if new_head_clr_flag = '1' then
-			next_state <= idle;
-		else
-			next_state <= waitHead;
-		end if;
-	
-	when set_corner_flags =>
 
-		corner_flag <= '1';
-		new_head_flag <= '1';
-		chc_flag <= '0';
-		new_head <= new_head;
-		corner <= corner;
-		
-		if corner_clr_flag = '1' then
-			next_state <= waitHead;
-		elsif new_head_clr_flag = '1' then
-			next_state <= waitCor;
+		if new_head_clr_flag = '1' then
+			next_state <= waitMove;
 		else
-			next_state <= set_corner_flags;
+			next_state <= waitHead;
 		end if;
 		
 	when waitCor =>
@@ -210,11 +140,9 @@ case state is
 		corner_flag <= '1';
 		new_head_flag <= '0';
 		chc_flag <= '0';
-		new_head <= new_head;
-		corner <= corner;
 		
 		if corner_clr_flag = '1' then
-			next_state <= idle;
+			next_state <= waitMove;
 		else
 			next_state <= waitCor;
 		end if;
@@ -224,22 +152,30 @@ case state is
 		corner_flag <= '0';
 		new_head_flag <= '1';
 		chc_flag <= '0';
-		new_head <= new_head;
-		corner <= corner;
 		
 		if new_head_clr_flag = '1' then
-			next_state <= idle;
+			next_state <= waitMove;
 		else
 			next_state <= waitHead;
+		end if;
+
+	when waitMove =>
+
+		corner_flag <= '0';
+		new_head_flag <= '0';
+		chc_flag <= '0';
+
+		if move = '0' then
+			next_state <= idle;
+		else
+			next_state <= waitMove;
 		end if;
 		
 	when others =>
 		corner_flag <= '0';
 		new_head_flag <= '0';
 		chc_flag <= '0';
-		new_head <= new_head;
-		corner <= corner;
-		next_state <= idle;		
+		next_state <= idle;
 end case;
 end process;
 
